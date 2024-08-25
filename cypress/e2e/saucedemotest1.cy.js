764cdb63b1a1311db58d4ab344e0000c16a89415
Franciszek Wielkoszynski <franciszek.wielkoszynski@orange.com>
2024-08-25 19:24:18 +0200

    test

--- cypress/e2e/saucedemotest1.cy.js
+++ cypress/e2e/saucedemotest1.cy.js
@@ -1,5 +1,6 @@
 describe('Pepper log in', () => {
   it('loges user in', () => {
+    //logowanie
     cy.visit('https://www.pepper.pl/')
     cy.get('[data-t="acceptAll"]').click()
     cy.get('button.button--toW5-square.space--ml-2.button.button--shape-circle.button--type-primary.button--mode-white').click()
@@ -9,6 +10,11 @@ describe('Pepper log in', () => {
     cy.get('span').contains(' Zaloguj Się ').click()
     cy.get('[data-t="userDropdown"]').click()
     cy.get('div>span').contains(' tescicki123 ')
+    cy.get('[data-t="userDropdown"]').click()
 
+    //komentowanie
+    cy.get('[class="thread-title "]').first().click()
+    cy.get('[placeholder="O czym teraz myślisz..."]').click()
+    
   })
 })
